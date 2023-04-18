9ab78451c3600c9358057a1c697970821dc0ca45
Aric-prog <hernandoaric@gmail.com>
2023-04-19 06:02:04 +1000

    added diagram

--- src/database/scripts/create_tables.sql
+++ src/database/scripts/create_tables.sql
@@ -1,9 +1,10 @@
+CREATE TYPE accountType AS ENUM ('sad', 'ok', 'happy');
 CREATE TABLE IF NOT EXISTS account(
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email TEXT NOT NULL, 
     username TEXT NOT NULL, 
     hashedPassword TEXT NOT NULL,
-    isAdmin BOOL NOT NULL,
+    accountType accountType NOT NULL,
     salt TEXT NOT NULL,
     UNIQUE(email)
 );
