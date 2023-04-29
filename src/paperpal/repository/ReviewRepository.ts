b5460833fba3fa183482f72e3a8cae4e3bbffaf8
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2023-04-29 16:55:57 +1000

    Update ReviewRepository.ts
    added remaining queries

--- src/paperpal/repository/ReviewRepository.ts
+++ src/paperpal/repository/ReviewRepository.ts
@@ -9,30 +9,50 @@ export default class ReviewRepository{
     
     // Note that this is accountId and not reviewer id. Join sequence are accountId > reviewerId > paperId
     async getReviewFromAccountAndPaper(accountId: number, paperId: number) : Promise<Review> {
+        `SELECT * 
+        FROM account
+        INNER JOIN reviewer ON account.id = reviewer.accountid
+        INNER JOIN paper ON reviewer.accountid = paper.id;
+`
         return;
     }
 
     async insertReview(review : Partial<Review>) {
+        `INSERT INTO review (paperrating, reviewrating, reviewerid, paperid)
+        VALUES (paperrating, reviewrating, reviewerid, paperid);`
         return;
     }
 
     async setPaperRating(reviewerId: number, paperId: number, rating: number) {
+        `UPDATE review
+        SET paperrating = INT
+        WHERE id = INT;`
         return;
     }
 
     async setReviewRating(reviewerId: number, paperId: number, rating: number) {
+        `UPDATE review
+        SET reviewrating = INT
+        WHERE id = INT;`
         return;
     }
 
     async addComment(reviewerId: number, paperId: number, comment: string): Promise<Comment> {
+        `INSERT INTO comment (comment, paperid, reviewerid)
+        VALUES('string', INT, INT);`
         return;
     }
 
     async deleteReview() {
+        `DELETE FROM review
+        WHERE review.id = INT;`
         return;
     }
 
     async getAllCommentsForPaper(paperId: number): Promise<Comment[]> {
+        `SELECT *
+        FROM comment
+        LEFT JOIN paper ON comment.paperid = paper.id;`
         return;
     }
     async getAllReviewsForPaper(paperId: number): Promise<Review[]>{
@@ -43,10 +63,16 @@ export default class ReviewRepository{
     }
 
     async updateReview() {
+        `UPDATE review
+        SET paperrating = INT, reviewrating = INT, 
+        reviewerrating = INT, paperid = INT
+        WHERE id = INT;`
         return;
     }
 
     async addReviewRating() {
+        `INSERT INTO review (reviewrating)
+        VALUES (INT);`
         return;
     }
     
