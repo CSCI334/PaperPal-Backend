433e6bdf389cd366f8d6ab61721194a4b3c09f59
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2023-05-06 01:34:34 +1000

    Update BidRepository.ts

--- src/paperpal/repository/BidRepository.ts
+++ src/paperpal/repository/BidRepository.ts
@@ -6,10 +6,15 @@ export default class BidRepository{
     constructor(@inject(DbService) private readonly db: DbService) {}
 
     async insertBid(reviewerId: number, paperId: number, bidAmount: number){
+        `INSERT INTO bids (bidamount, reviewid, paperid)
+        VALUES (bidamount, reviewid, paperid)`;
         return;
     }
 
     async getBidsForAccount(reviewerId: number) {
+        `SELECT *
+        FROM bids
+        WHERE id = reviewerId`;
         return;
     }
 }
