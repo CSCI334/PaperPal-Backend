63a289d1a34ea716df4da450b3171a443fbe0824
Aric-prog <hernandoaric@gmail.com>
2023-05-26 04:41:44 +1000

    ok i lied

--- src/database/scripts/create_tables.sql
+++ src/database/scripts/create_tables.sql
@@ -104,20 +104,20 @@ CREATE TABLE IF NOT EXISTS review(
 INSERT INTO account(email, username, hashedPassword, salt, accountType, accountStatus) 
 VALUES('admin@email.com', 'Admin', 'e3d22c3c4ea69d5612ffab07e55d9a40e46f187c3bdee401a71019cbae4e1b84', '89946a', 'ADMIN', 'ACCEPTED');
 
-INSERT INTO conference(conferenceName,conferenceLocation,submissionDeadline,biddingDeadline, reviewDeadline,announcementTime)
-VALUES('The conference of all time','placeholder location','2023-06-14', '2023-06-15', '2023-06-16', '2023-06-17');
-INSERT INTO account(email, username, hashedPassword, salt, accountType, accountStatus, conferenceId) 
-VALUES('reviewer@email.com', 'Reviewer', 'e3d22c3c4ea69d5612ffab07e55d9a40e46f187c3bdee401a71019cbae4e1b84', '89946a', 'REVIEWER', 'ACCEPTED', 1);
-INSERT INTO reviewer(accountId, bidPoints, paperWorkload)
-VALUES(2, 15, 5);
-INSERT INTO account(email, username, hashedPassword, salt, accountType, accountStatus, conferenceId) 
-VALUES('author@email.com', 'Author', 'e3d22c3c4ea69d5612ffab07e55d9a40e46f187c3bdee401a71019cbae4e1b84', '89946a', 'AUTHOR', 'ACCEPTED', 1);
-INSERT INTO author(accountId)
-VALUES(3);
-INSERT INTO account(email, username, hashedPassword, salt, accountType, accountStatus, conferenceId) 
-VALUES('chair@email.com', 'Chair', 'e3d22c3c4ea69d5612ffab07e55d9a40e46f187c3bdee401a71019cbae4e1b84', '89946a', 'CHAIR', 'ACCEPTED', 1);
-INSERT INTO chair(accountId)
-VALUES(4);
+-- INSERT INTO conference(conferenceName,conferenceLocation,submissionDeadline,biddingDeadline, reviewDeadline,announcementTime)
+-- VALUES('The conference of all time','placeholder location','2023-06-14', '2023-06-15', '2023-06-16', '2023-06-17');
+-- INSERT INTO account(email, username, hashedPassword, salt, accountType, accountStatus, conferenceId) 
+-- VALUES('reviewer@email.com', 'Reviewer', 'e3d22c3c4ea69d5612ffab07e55d9a40e46f187c3bdee401a71019cbae4e1b84', '89946a', 'REVIEWER', 'ACCEPTED', 1);
+-- INSERT INTO reviewer(accountId, bidPoints, paperWorkload)
+-- VALUES(2, 15, 5);
+-- INSERT INTO account(email, username, hashedPassword, salt, accountType, accountStatus, conferenceId) 
+-- VALUES('author@email.com', 'Author', 'e3d22c3c4ea69d5612ffab07e55d9a40e46f187c3bdee401a71019cbae4e1b84', '89946a', 'AUTHOR', 'ACCEPTED', 1);
+-- INSERT INTO author(accountId)
+-- VALUES(3);
+-- INSERT INTO account(email, username, hashedPassword, salt, accountType, accountStatus, conferenceId) 
+-- VALUES('chair@email.com', 'Chair', 'e3d22c3c4ea69d5612ffab07e55d9a40e46f187c3bdee401a71019cbae4e1b84', '89946a', 'CHAIR', 'ACCEPTED', 1);
+-- INSERT INTO chair(accountId)
+-- VALUES(4);
 
 CREATE VIEW paperconference AS
 SELECT paper.id AS paperid, conference.id AS conferenceId, account.id AS accountId, paper.paperstatus, paper.title, conference.conferencename,
